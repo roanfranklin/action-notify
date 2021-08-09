11f16c1d35b8f398d13b6f902b0111fff5f6835b
Roan Franklin <roanfranklin@gmail.com>
2021-08-09 18:21:43 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ if [[ ! -z "$DISCORD_WEBHOOK_ID" ] && [ ! -z "$DISCORD_WEBHOOK_TOKEN" ]]; then
     sh -c "curl --silent --show-error --fail -X POST --data '{\"content\": \"${TEXT_MESSAGE}\"}' --header \"Content-Type:application/json\" \"https://discord.com/api/webhooks/${DISCORD_WEBHOOK_ID}/${DISCORD_WEBHOOK_TOKEN}\""
 fi
 
-if [[ ! -z "$TELEGRAM_WEBHOOK_ID" ] && [ ! -z "$TELEGRAM_WEBHOOK_TOKEN" ]]; then
-    sh -c "curl --silent --show-error --fail -X POST -H 'Content-Type: application/json' -d '{\"chat_id\": \"${TELEGRAM_WEBHOOK_ID}\", \"text\": \"${TEXT_MESSAGE}\", \"disable_notification\": ${TELEGRAM_DISABLE_NOTIFICATION}' https://api.telegram.org/bot${TELEGRAM_WEBHOOK_TOKEN}/sendMessage"
-fi
-
-if [[ ! -z "$SLACK_WEBHOOK_ID" ]]; then
-    sh -c "curl --silent --show-error --fail -H \"Content-type: application/json\" --data '{\"channel\":\"${SLACK_WEBHOOK_ID}\",\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"${TEXT_MESSAGE}\"}}]}' -H \"Authorization: Bearer xoxb-not-a-real-token-this-will-not-work\" -X POST https://slack.com/api/chat.postMessage"
-fi
+#if [[ ! -z "$TELEGRAM_WEBHOOK_ID" ] && [ ! -z "$TELEGRAM_WEBHOOK_TOKEN" ]]; then
+#    sh -c "curl --silent --show-error --fail -X POST -H 'Content-Type: application/json' -d '{\"chat_id\": \"${TELEGRAM_WEBHOOK_ID}\", \"text\": \"${TEXT_MESSAGE}\", \"disable_notification\": ${TELEGRAM_DISABLE_NOTIFICATION}' https://api.telegram.org/bot${TELEGRAM_WEBHOOK_TOKEN}/sendMessage"
+#fi
+#
+#if [[ ! -z "$SLACK_WEBHOOK_ID" ]]; then
+#    sh -c "curl --silent --show-error --fail -H \"Content-type: application/json\" --data '{\"channel\":\"${SLACK_WEBHOOK_ID}\",\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"${TEXT_MESSAGE}\"}}]}' -H \"Authorization: Bearer xoxb-not-a-real-token-this-will-not-work\" -X POST https://slack.com/api/chat.postMessage"
+#fi
 
 #    sh -c "curl --silent --show-error --fail $*"
