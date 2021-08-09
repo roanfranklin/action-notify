17d79d1d2d6d5bd7d7be85493430ce18f2167420
Roan Franklin <roanfranklin@gmail.com>
2021-08-09 18:23:10 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 set -e
 : #{TELEGRAM_DISABLE_NOTIFICATION:=true}
 
-if [[ ! -z "$DISCORD_WEBHOOK_ID" ] && [ ! -z "$DISCORD_WEBHOOK_TOKEN" ]]; then
-    sh -c "curl --silent --show-error --fail -X POST --data '{\"content\": \"${TEXT_MESSAGE}\"}' --header \"Content-Type:application/json\" \"https://discord.com/api/webhooks/${DISCORD_WEBHOOK_ID}/${DISCORD_WEBHOOK_TOKEN}\""
-fi
+#if [[ ! -z "$DISCORD_WEBHOOK_ID" ] && [ ! -z "$DISCORD_WEBHOOK_TOKEN" ]]; then
+sh -c "curl --silent --show-error --fail -X POST --data '{\"content\": \"${TEXT_MESSAGE}\"}' --header \"Content-Type:application/json\" \"https://discord.com/api/webhooks/${DISCORD_WEBHOOK_ID}/${DISCORD_WEBHOOK_TOKEN}\""
+#fi
 
 #if [[ ! -z "$TELEGRAM_WEBHOOK_ID" ] && [ ! -z "$TELEGRAM_WEBHOOK_TOKEN" ]]; then
 #    sh -c "curl --silent --show-error --fail -X POST -H 'Content-Type: application/json' -d '{\"chat_id\": \"${TELEGRAM_WEBHOOK_ID}\", \"text\": \"${TEXT_MESSAGE}\", \"disable_notification\": ${TELEGRAM_DISABLE_NOTIFICATION}' https://api.telegram.org/bot${TELEGRAM_WEBHOOK_TOKEN}/sendMessage"
